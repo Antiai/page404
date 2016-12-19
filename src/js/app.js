b3c9150bcf6b1969f1a8474ff579d95901e24556
Anton Ershov <anton.ershov@umbrella-web.com>
2016-12-19 15:26:26 +0300

    #3 Add search form. Also add CSS  animation for menu and popup

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", ev => {
     hamburgerBtn.classList.remove("hamburger-btn-cross");
   }
 
+
   hamburgerBtn.addEventListener("click", function(event) {
     event.preventDefault();
     hamburgerBtn.classList.toggle("hamburger-btn-cross");
@@ -28,6 +29,7 @@ document.addEventListener("DOMContentLoaded", ev => {
     if (event.keyCode === 27) {
       hamburgerBtn.classList.remove("hamburger-btn-cross");
       menu.classList.remove("menu-items-open");
+      popup.classList.remove("popup-open");
     };
   });
 
@@ -60,4 +62,36 @@ document.addEventListener("DOMContentLoaded", ev => {
       };
     });
   };
+
+  //#Search
+
+  let popup = document.querySelector(".popup");
+  let searchBtn = document.querySelector(".menu-search-btn");
+  let searchLink = document.querySelector(".main-message-link");
+
+  function openSearch() {
+    event.preventDefault();
+    if (!popup.classList.contains("popup-open")) {
+      popup.classList.add("popup-open");
+    }
+  }
+
+  function closeSearch() {
+    event.preventDefault();
+    if (event.target.classList.contains("popup-open")) {
+      popup.classList.remove("popup-open");
+    }
+  }
+
+  searchBtn.addEventListener("click", function(event) {
+    openSearch();
+  });
+
+  searchLink.addEventListener("click", function(event) {
+    openSearch();
+  });
+
+  popup.addEventListener("click", function(event) {
+    closeSearch();
+  });
 });
