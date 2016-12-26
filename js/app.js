261b8b656a97d7ca339a6068021bbac0a57ba2f6
Anton Ershov <awakening1985@gmail.com>
2016-12-26 20:03:07 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function(event) {
 
+document.addEventListener("DOMContentLoaded", function (event) {
   //#Menu
 
   var hamburgerBtn = document.querySelector(".hamburger-btn");
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     hamburgerBtn.classList.remove("hamburger-btn-cross");
   }
 
-  hamburgerBtn.addEventListener("click", function(event) {
+  hamburgerBtn.addEventListener("click", function (event) {
     event.preventDefault();
     hamburgerBtn.classList.toggle("hamburger-btn-cross");
     menu.classList.toggle("menu-items-open");
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   });
 
-  window.addEventListener("keydown", function(event) {
+  window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
       hamburgerBtn.classList.remove("hamburger-btn-cross");
       menu.classList.remove("menu-items-open");
@@ -44,9 +44,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var pinterestBtn = document.querySelectorAll(".share-btn");
   var pics = document.querySelectorAll(".popular-content-image");
   var popContent = document.querySelector(".popular-content-wrapper");
+  var popLink = document.querySelectorAll(".popular-content-link");
+  var popText = document.querySelectorAll(".popular-content-link-arrow");
 
   function showPin(arg) {
-    pics[arg].addEventListener("mouseenter", function() {
+    pics[arg].addEventListener("mouseenter", function () {
       if (!pinterestBtn[arg].classList.contains("share-btn-hover")) {
         pinterestBtn[arg].classList.add("share-btn-hover");
       };
@@ -54,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function hidePin(arg) {
-    pics[arg].addEventListener("mouseleave", function() {
+    pics[arg].addEventListener("mouseleave", function () {
       if (pinterestBtn[arg].classList.contains("share-btn-hover")) {
         pinterestBtn[arg].classList.remove("share-btn-hover");
       };
@@ -62,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function lockPin(arg) {
-    pinterestBtn[arg].addEventListener("mouseenter", function() {
+    pinterestBtn[arg].addEventListener("mouseenter", function () {
       if (!pinterestBtn[arg].classList.contains("share-btn-hover")) {
         pinterestBtn[arg].classList.add("share-btn-hover");
       };
@@ -70,18 +72,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function releasePin(arg) {
-    pinterestBtn[arg].addEventListener("mouseleave", function() {
+    pinterestBtn[arg].addEventListener("mouseleave", function () {
       if (pinterestBtn[arg].classList.contains("share-btn-hover")) {
         pinterestBtn[arg].classList.remove("share-btn-hover");
       };
     });
   }
 
+  //##Change attributes for Buttons
+
+  function setLinkAttr(arg) {
+    var title = popLink[arg].innerHTML;
+    var text = popText[arg].innerHTML;
+    var desc = title + "\r\n" + text;
+    var mediaLink = window.location.origin + "/" + pics[arg].getAttribute("src");
+    var url = popLink[arg].getAttribute("href");
+
+    function setProp(attr, value) {
+      if (!pinterestBtn[arg].hasAttribute(attr)) {
+        pinterestBtn[arg].setAttribute(attr, value);
+      }
+    }
+
+    pinterestBtn[arg].setAttribute("href", "https://www.pinterest.com/pin/create/button/");
+
+    setProp("data-pin-custom", "true");
+    setProp("data-pin-do", "buttonPin");
+    setProp("data-pin-description", desc);
+    setProp("data-pin-media", mediaLink);
+    setProp("data-pin-url", url);
+  }
+
+  //##Call pinterest functions
+
   for (var i = 0; i < pics.length; i++) {
     showPin(i);
     hidePin(i);
     lockPin(i);
     releasePin(i);
+    setLinkAttr(i);
   };
 
   //#Search
@@ -97,18 +126,71 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   }
 
-  searchBtn.addEventListener("click", function(event) {
+  searchBtn.addEventListener("click", function (event) {
     openSearch(event);
   });
 
-  searchLink.addEventListener("click", function(event) {
+  searchLink.addEventListener("click", function (event) {
     openSearch(event);
   });
 
-  popup.addEventListener("click", function(event) {
+  popup.addEventListener("click", function (event) {
     event.preventDefault();
     if (event.target.classList.contains("popup-open")) {
       popup.classList.remove("popup-open");
     }
   });
+
+  //#Swiper init
+
+  var mySwiper = new Swiper(".swiper-container", {
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 1,
+    paginationClickable: true,
+    spaceBetween: 17,
+    centeredSlides: true,
+    autoplay: 2500,
+    autoplayDisableOnInteraction: true,
+    loop: true
+  });
+
+  //##Mediaqueries for swiper
+
+  if (matchMedia) {
+    var mq = window.matchMedia("(min-width: 768px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+  }
+
+  function WidthChange(mq) {
+    if (mq.matches) {
+      mySwiper.destroy(true, true);
+    }
+  }
+
+  if (matchMedia) {
+    var mqmax = window.matchMedia("(max-width: 767px)");
+    mqmax.addListener(WidthChanges);
+    WidthChanges(mqmax);
+  }
+
+  function WidthChanges(mqmax) {
+    if (mqmax.matches) {
+      mySwiper = new Swiper(".swiper-container", {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 17,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: true,
+        loop: true
+      });
+    }
+  }
 });
+//# sourceMappingURL=app.js.map
